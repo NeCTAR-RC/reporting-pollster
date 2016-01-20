d5e15363f1a2b7006e734a9ca91ee9bfb2f74f61
Simon Fowler <simon.fowler@anu.edu.au>
2016-01-21 10:37:33 +1100

    Remove pip-related setup configuration.
    Moving to requiring mysql-connector-python-rf because the
    mysql-connector-python pip package is defunct. This is only relevant
    during testing via jenkins - elsewhere all dependencies are managed via
    packages.
    
    Change-Id: Ifbb68276b99ad4cc37b0226a59cde1cdac1dfb02

--- setup.py
+++ setup.py
@@ -1,15 +1,9 @@
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
-from pip.index import PackageFinder
 
 version = '0.1.0'
 
-finder = PackageFinder(find_links=[],
-                       session=False,
-                       index_urls=["http://pypi.python.org/simple/"])
-requirements = parse_requirements('requirements.txt',
-                                  session=False,
-                                  finder=finder)
+requirements = parse_requirements("requirements.txt", session=False)
 
 setup(name='reporting-pollster',
       version=version,
@@ -22,4 +16,4 @@ setup(name='reporting-pollster',
       install_requires=[str(r.req) for r in requirements],
       scripts=['reporting-pollster'],
       test_suite="tests.test_all"
-)
+      )
